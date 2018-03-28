782fc264bea4f75661513f214b95cc14ec885491
Rheii <er_eldrinrei@yahoo.com>
2018-03-28 19:06:45 +0800

    First Commit
    All files committed to repo

---
+++ app/modules/auth/routes.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var loginRouter = express.Router();
+var signupRouter = express.Router();
+var logoutRouter = express.Router();
+
+var authMiddleware = require('./middlewares/auth');
+
+loginRouter.route('/')
+    .get(authMiddleware.noAuthed, (req, res) => {
+        res.render('auth/views/login', req.query);
+    })
+    .post((req, res) => {
+        var db = require('../../lib/database')();
+
+        db.query(`SELECT * FROM users WHERE email="${req.body.email}"`, (err, results, fields) => {
+            if (err) throw err;
+            if (results.length === 0) return res.redirect('/login?incorrect');
+
+            var user = results[0];
+
+            if (user.password !== req.body.password) return res.redirect('/login?incorrect');
+
+            delete user.password;
+
+            req.session.user = user;
+
+            return res.redirect('/');
+        });
+    });
+
+logoutRouter.get('/', (req, res) => {
+    req.session.destroy(err => {
+        if (err) throw err;
+        res.redirect('/login');
+    });
+});
+
+signupRouter.route('/')
+.get((req, res) => {
+  res.render("home/views/signup")
+});
+
+exports.login = loginRouter;
+exports.signup = signupRouter;
+exports.logout = logoutRouter;
