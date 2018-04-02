4c728b49b2d0a8f56213b74638dc4323db8fe245
Rheii <er_eldrinrei@yahoo.com>
2018-04-02 09:24:41 +0800

    fixed image upload now saving

--- app/modules/home/routes.js
+++ app/modules/home/routes.js
@@ -7,7 +7,7 @@ router.use(authMiddleware.hasAuth);
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/img')
+    cb(null, 'public/img/uploads')
   },
   filename: function (req, file, cb) {
     
